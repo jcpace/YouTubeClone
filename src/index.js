614f0403061d5d9a019c573ad87a26d50ae1fcc3
Joshua Pace <joshpace@gmail.com>
2018-01-25 18:48:13 -0800

    install eslint / prettier

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const API_KEY = 'AIzaSyCrxaj2bzdJQ-44FdAjZzTUygCTNLfYNM0'
 const App = () => {
-  return <div>hellso</div>
+  return <div>hello</div>
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
 
