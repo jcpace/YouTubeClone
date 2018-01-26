e36a566a1479e95458539173688d93652cc34682
Joshua Pace <joshpace@gmail.com>
2018-01-26 14:31:14 -0800

    add videolist

---
+++ src/components/video_list_item.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VideoListItem = props => <li>Video</li>;
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCrxaj2bzdJQ-44FdAjZzTUygCTNLfYNM0';
 
-YTSearch({
-  key: API_KEY,
-  term: 'climbing'
-}, data => { console.log(data);});
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      videos: []
-    }
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'climbing'}, videos => { this.setState({ videos }); console.log(videos);});
   }
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     ) 
   }
