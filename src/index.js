3344358aaa9a50beca33cb4be39c00bf2de6a499
Joshua Pace <joshpace@gmail.com>
2018-01-25 19:28:38 -0800

    refactor of search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyCrxaj2bzdJQ-44FdAjZzTUygCTNLfYNM0'
 const App = () => {
-  return <div>hello</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
-registerServiceWorker();
 
