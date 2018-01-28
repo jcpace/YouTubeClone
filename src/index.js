7def6c871c91cf126645ade4a133d989bce61600
Joshua Pace <joshpace@gmail.com>
2018-01-27 17:51:50 -0800

    add loading msg null props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar.js';
-import VideoList from './components/video_list.js';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCrxaj2bzdJQ-44FdAjZzTUygCTNLfYNM0';
 
 
@@ -12,13 +13,14 @@ export default class App extends Component {
     super(props);
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'climbing'}, videos => { this.setState({ videos }); console.log(videos);});
+    YTSearch({ key: API_KEY, term: 'climbing'}, videos => { this.setState({ videos }); console.log(videos, 'videos from YTSearch');});
   }
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail videos={this.state.videos}/>
         <VideoList videos={this.state.videos}/>
       </div>
     ) 
