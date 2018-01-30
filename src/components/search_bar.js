da3d5b9564b07726e79bc3c46028a8cacb1d91db
Joshua Pace <joshpace@gmail.com>
2018-01-29 16:24:17 -0800

    controlled component search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -16,9 +16,14 @@ export default class SearchBar extends Component {
 
   render() {
     return (
-      <div>
-        <input value={this.state.value} onChange={this.handleChange} />
+      <div className='search-bar'>
+        <input 
+        value={this.state.term} 
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+  onInputChange(term) {
+
   }
 }
